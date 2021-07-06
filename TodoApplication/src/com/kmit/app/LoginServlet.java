76c45fd02c90a8faf9bbc00d7a737cfce827fa98
Mamta-Reddy <61815173+Mamta-Reddy@users.noreply.github.com>
2021-07-06 09:39:09 +0530

    Add files via upload

---
+++ TodoApplication/src/com/kmit/app/LoginServlet.java
@@ -0,0 +1,86 @@
+package com.kmit.app;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("view/login.html").forward(request, response);
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("name");
+		String pass=request.getParameter("password");
+		
+		ServletContext sc = getServletContext();
+		String driverName=sc.getInitParameter("driverName"); 
+		String driverUrl=sc.getInitParameter("driverUrl"); 
+		String username=sc.getInitParameter("username"); 
+		String password=sc.getInitParameter("password");
+		
+		 Connection conn=null;
+		 PreparedStatement pstmt =null;
+		 ResultSet rs = null;
+		 
+		 try {
+			 Class.forName(driverName);
+			 conn=DriverManager.getConnection(driverUrl,username,password);
+			 
+			 String sql= "select * from login where username = ? and password = ?";
+			 pstmt=conn.prepareStatement(sql);
+			 pstmt.setString(1,name);
+			 pstmt.setNString(2, pass);
+			 rs=pstmt.executeQuery();
+			 
+			 //if user is available
+			 if(rs != null && rs.next()) {
+				 //user is available
+				 HttpSession session=request.getSession();
+				 session.setAttribute("user_id", rs.getInt("user_id"));
+				 
+				 response.sendRedirect("TodoListServlet");
+			 }
+			 else {
+				 request.setAttribute("error", "Not able to login : User not found");
+					request.getRequestDispatcher("ErrorServlet").forward(request, response);
+			 }
+			 
+		 }catch(Exception e) {
+			 e.printStackTrace();
+			 request.setAttribute("error", "Not able to login "+e.getMessage());
+			request.getRequestDispatcher("ErrorServlet").forward(request, response);
+		 }
+		 finally {
+			 try {
+				 	pstmt.close();
+					rs.close();
+					conn.close();
+			 }catch(Exception e) {
+					e.printStackTrace();
+					//System.out.println("Connection failed");
+					request.setAttribute("error", "Not able to login "+e.getMessage());
+					request.getRequestDispatcher("ErrorServlet").forward(request, response);
+				
+			 }
+			 
+		 }
+		 
+	}
+
+}
